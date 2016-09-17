fb18fa89bcb70e4a5850b7d45fc24b577db6fe4c
Julian SPRING <jspring@e5r9p9.jnb.42.fr>
2016-09-17 12:43:12 +0200

    pushing the folders

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="/css/mystyle.css">
+	<title>Penislands | Home</title>
+</head>
+<body>
+ <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive" src="img/profile.png" alt="">
+                    <div class="intro-text">
+                        <span class="name">Start Bootstrap</span>
+                        <hr class="star-light">
+                        <span class="skills">Web Developer - Graphic Artist - User Experience Designer</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
