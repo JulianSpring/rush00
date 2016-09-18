76f87aec4451972383f3080f878ac27a9bf289e8
Julian SPRING <jspring@e5r10p7.jnb.42.fr>
2016-09-18 18:08:26 +0200

    store.php working

--- html/store.php
+++ html/store.php
@@ -32,26 +32,57 @@
   </div> 
 <!-- php code -->
 <?php
-
+  include(../php/cart.php);
   $conn = mysqli_connect("127.0.0.1", "root", "123456");
   if (!$conn)
     die("Connection failed: " . msyqli_connect_error());
 
-   mysqli_select_db($conn, "rush00");
-   $sql = "SELECT * FROM tbl_store";
-   $result = mysqli_query($conn, $sql) or die("Query to show fields from table failed");
+    mysqli_select_db($conn, "rush00");
+    $sql = "SELECT * FROM tbl_store";
+    $result = mysqli_query($conn, $sql) or die("Query to show fields from table failed");
+   
+    $types = array();
+    $num_rows = mysqli_num_rows($result);
+       if ($num_rows > 0)
+       {
+           while ($rows = mysqli_fetch_array($result)) 
+           {
+              $URLS[] = array('id' => $rows['id'], 'category' => $rows['category'], 'name' => $rows['name'], 'price'=> $rows['price'], 'image' => $rows['image'] );
+           }
 
-   $types = array();
-   print_r($result);
-   $types = array();
-
-while ($row = $result->fetch())
-{
-  $URLS[] = array('Category' => $row['category'], 'name' => $row['name'], 'price'=> $row['price'], 'image' =>$row[] );
-}
-
+       }
 ?>
 <!-- Grid for products -->
+  <div id="table_admin" class="span7">
+        <h3>Discover</h3>
+
+        <table class="table table-striped table-condensed">
+
+                <thead>
+                <tr>
+                <th>Category</th>
+                <th>Name</th>
+                <th>Price </th> 
+                </tr>
+                </thead>
+            <tbody>
+            <?php 
+                foreach ($URLS as $URL){
+                    echo'<tr>'; 
+                    echo'<td>'. $URL['category']."</td>";
+                    echo'<td>'. $URL['name'].'</td>';
+                    echo'<td>'. $URL['price'].'</td>';
+                    echo'<td> <input type="submit" name="modify" value="Modify" /> </td>';
+                    echo'<tr>';
+                }
+            ?>
+            </tbody>
+
+
+        </table>
+
+    </div>
+
 
 </body>
 </html>
--- php/cart.php
+++ php/cart.php
@@ -7,28 +7,32 @@
  * TODO HAS NOT BEEN TESTED
  */
 
-function add_to_cart($email, $item_id)
+if( isset( $_REQUEST['modify'] ))
 {
-	$file = "../data/cart";
-	$cart = array();
-	$cart = unserialize(file_get_contents($file));
-	$len = 0;
-	foreach ($cart as $item)
-		$len++;
-
-	/*
-	 * Fetching all the details of the item for the database
-	 */
-	$conn = mysqli_connect("127.0.0.1", "root", "123456");
-	mysqli_select_db($conn, "rush00");
-	if (!($query = mysqli_query($conn, "SELECT * FROM tbl_products WHERE id = '$item_id';")))
+	add_to_cart();
+	function add_to_cart($email, $item_id)
 	{
-		$row = mysql_fetch_assoc($query);
-		$cart[$len] = $row;
-		file_put_contents($file, serialize($cart));
+		$file = "../data/cart";
+		$cart = array();
+		$cart = unserialize(file_get_contents($file));
+		$len = 0;
+		foreach ($cart as $item)
+			$len++;
+
+		/*
+		 * Fetching all the details of the item for the database
+		 */
+		$conn = mysqli_connect("127.0.0.1", "root", "123456");
+		mysqli_select_db($conn, "rush00");
+		if (!($query = mysqli_query($conn, "SELECT * FROM tbl_products WHERE id = '$item_id';")))
+		{
+			$row = mysql_fetch_assoc($query);
+			$cart[$len] = $row;
+			file_put_contents($file, serialize($cart));
+		}
+		else
+			echo "Add to cart query error " . mysql_error($conn) . "\n";
 	}
-	else
-		echo "Add to cart query error " . mysql_error($conn) . "\n";
 }
 
 
